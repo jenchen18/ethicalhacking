11452bb38d49070fbfc36e0a5f42939184d37a14
jenchen18 <jen.chen318@gmail.com>
2021-04-24 15:24:01 -0400

    Added wget and nginx restart

--- source.py
+++ source.py
@@ -1,6 +1,26 @@
-# Some file
+import os
+
+SITE = "www.hackthissite.org"
+
+def nginx_config():
+	path = "/etc/nginx/nginx.conf"
+	with open(path, 'r') as file:
+		text = file.readlines()
+	location = 0
+	for i in range(len(text)):
+		if 'location /' in text[i].strip() and '#' not in text[i]:
+			location = i + 1
+	broken_path = (text[location].split()[1][:-1]).split("/")
+	broken_path[len(broken_path)-2] = SITE
+	text[location] = "\t\t\t" + " ".join(text[location].split()[:-1]) + " " + "/".join(broken_path) + ";\n"
+	with open(path, 'w') as file:
+		file.writelines(text)
+	return 0
 
 def main():
+	os.system("wget --level=1 --recursive --page-requisites --no-parent --convert-links --adjust-extension --no-clobber -e robots=off " + SITE)
+	nginx_config()
+	os.system("systemctl restart nginx")
 	return 0
 
 if __name__ == '__main__':
